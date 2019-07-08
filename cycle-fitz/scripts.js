42a3d93e91c57a82cfa996870f5bca0b4564951f
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-08 16:00:50 -0500

    making separate html for cart

--- cycle-fitz/scripts.js
+++ cycle-fitz/scripts.js
@@ -1,3 +1,6 @@
+
+const EventEmitter = require("events");
+
 const inputField = document.getElementById("input-text");
 
 const inputDiv = document.getElementById("user-input");
@@ -6,17 +9,31 @@ const cart = [];
 
 const cartListDiv = document.getElementById("cart-list");
 
+const emitter = new events.EventEmitter();
+
 const cartAdd = e => {
-  cartItem = document.createElement("span");
-  cartItem.setAttribute("class", "cart-item");
-  removeButton = document.createElement("button");
-  removeButton.setAttribute("onclick", "removeFromCart(this)");
-  removeButton.innerHTML = `remove`;
-  removeButton.setAttribute("class", "removeButton");
-  cartItem.innerHTML = `${e.id}`;
-  cartListDiv.appendChild(cartItem);
-  cartItem.appendChild(removeButton);
+  // cartItem = document.createElement("span");
+  // cartItem.setAttribute("class", "cart-item");
+  // removeButton = document.createElement("button");
+  // removeButton.setAttribute("onclick", "removeFromCart(this)");
+  // removeButton.innerHTML = `remove`;
+  // removeButton.setAttribute("class", "removeButton");
+  // cartItem.innerHTML = `${e.id}`;
+  // cartListDiv.appendChild(cartItem);
+  // cartItem.appendChild(removeButton);
+  cart.push(e.id);
+  emitter.emit("addCart");
 };
+
+const updateCart = () => {
+  const cartQuantity = document.getElementById("cart-quantity");
+  currentquanity = cart.length;
+  quantity = document.createElement("span");
+  quantity.innerHTML = currentquanity;
+  cartQuantity.appendChild(quantity);
+};
+
+document.addEventListener("addCart", updateCart());
 
 const removeFromCart = e => {
   const cart = e.parentNode.parentNode;
