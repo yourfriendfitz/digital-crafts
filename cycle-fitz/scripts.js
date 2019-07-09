dd7a2114063a04902f2c4425ee17a089ca88d88f
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-09 09:16:57 -0500

    fixed cart styling

--- cycle-fitz/scripts.js
+++ cycle-fitz/scripts.js
@@ -1,39 +1,31 @@
-
-const EventEmitter = require("events");
-
 const inputField = document.getElementById("input-text");
 
 const inputDiv = document.getElementById("user-input");
+
+const navbar = document.getElementById("navbar");
 
 const cart = [];
 
 const cartListDiv = document.getElementById("cart-list");
 
-const emitter = new events.EventEmitter();
-
 const cartAdd = e => {
-  // cartItem = document.createElement("span");
-  // cartItem.setAttribute("class", "cart-item");
-  // removeButton = document.createElement("button");
-  // removeButton.setAttribute("onclick", "removeFromCart(this)");
-  // removeButton.innerHTML = `remove`;
-  // removeButton.setAttribute("class", "removeButton");
-  // cartItem.innerHTML = `${e.id}`;
-  // cartListDiv.appendChild(cartItem);
-  // cartItem.appendChild(removeButton);
   cart.push(e.id);
-  emitter.emit("addCart");
+  updateCart();
 };
 
 const updateCart = () => {
   const cartQuantity = document.getElementById("cart-quantity");
   currentquanity = cart.length;
   quantity = document.createElement("span");
-  quantity.innerHTML = currentquanity;
+  quantity.innerHTML = `(${currentquanity})`;
+  quantity.setAttribute("id", "itemsInCart");
+  cartQuantity.childNodes.forEach(node => {
+    if (node.id === "itemsInCart") {
+      cartQuantity.removeChild(node);
+    }
+  });
   cartQuantity.appendChild(quantity);
 };
-
-document.addEventListener("addCart", updateCart());
 
 const removeFromCart = e => {
   const cart = e.parentNode.parentNode;
