9eca760db597f3d3070666d2c098e39b389c83fc
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-12 15:44:56 -0500

    refactored

--- python-challenges/prime_check.py
+++ python-challenges/prime_check.py
@@ -4,10 +4,10 @@ number = int(input("Enter a number: "))
 if number > 1:
    for integer in range(2,number):
        if (number % integer) == 0:
-           print(number,"is not a prime number")
-           print(integer,"times",number//integer,"is",number)
+           print(f"{number} is not a prime number")
+           print(f"{integer} times {number//integer} is {number}")
            break
    else:
-       print(number,"is a prime number")
+       print(f"{number} is a prime number")
 else:
-   print(number,"is not a prime number")
+   print(f"{number}is not a prime number")
