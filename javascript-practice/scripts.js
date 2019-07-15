c00d740e4f6ea36a2deccb257c8c2c0b3dae2373
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-15 09:37:50 -0500

    added functionality to onclick changeText

--- javascript-practice/scripts.js
+++ javascript-practice/scripts.js
@@ -1,11 +1,21 @@
 const log = console.log;
 
-const buttonOne = document.getElementById("one");
-const buttonTwo = document.getElementById("two");
-const buttonThree = document.getElementById("three");
+const paragraphOne = document.getElementById("one");
+const paragraphTwo = document.getElementById("two");
+const paragraphThree = document.getElementById("three");
+
+const longText =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+const shortText = "Lorem ipsum dolor sit amet.";
 
 const changeText = e => {
-    console.log(e)
-}
-
-buttonOne.addEventListener("click", changeText(e))
+  paragraphId = e.parentNode.childNodes[1].id;
+  if (paragraphId === "one") {
+    if (paragraphOne.innerHTML === longText) {
+      paragraphOne.innerHTML = shortText;
+    } else {
+      paragraphOne.innerHTML = longText;
+    }
+  }
+};
