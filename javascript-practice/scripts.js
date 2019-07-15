42a1f5a363fda9a6fed152d8ebcb68a95f609de0
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-15 09:19:57 -0500

    testing change text

--- javascript-practice/scripts.js
+++ javascript-practice/scripts.js
@@ -1,5 +1,10 @@
-const log = console.log
+const log = console.log;
 
-const buttonOne = document.getElementById("one")
-const buttonTwo = document.getElementById("two")
-const buttonThree = document.getElementById("three")
+const buttonOne = document.getElementById("one");
+const buttonTwo = document.getElementById("two");
+const buttonThree = document.getElementById("three");
+
+const changeText = e => {
+  log(e);
+};
+
