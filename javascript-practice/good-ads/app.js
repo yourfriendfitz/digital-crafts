eddc0b79c5caba8013b1b0aeb0ecdac9e267824f
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-16 10:59:21 -0500

    solved with template literal and outerHTML

--- javascript-practice/good-ads/app.js
+++ javascript-practice/good-ads/app.js
@@ -1,6 +1,12 @@
-
 // call the getRandomAd function
 
 // get the adImage by id
 
-// set the src property to the new random ad image  
+// set the src property to the new random ad image
+adElement = document.getElementById("adImage");
+
+const changeAd = () => {
+  adElement.outerHTML = `<img id="adImage" src="${getRandomAd()}" style="width:300px;height:300px" />`;
+};
+
+document.addEventListener("load", changeAd());
