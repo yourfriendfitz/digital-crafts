3744b555a70ecf8337ad395027e44d15a1757032
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-16 09:44:34 -0500

    added a clear boxes function

--- javascript-practice/dom-manipulation/scripts.js
+++ javascript-practice/dom-manipulation/scripts.js
@@ -4,4 +4,12 @@ box1 = document.getElementById("box1");
 box2 = document.getElementById("box2");
 box3 = document.getElementById("box3");
 
+boxArray = [box1, box2, box3];
+
+const clearBoxes = elementsArray => {
+  elementsArray.forEach(element => {
+    element.innerHTML = "";
+  });
+};
+
 const numbers = [1, 2, 3, 4, 5];
