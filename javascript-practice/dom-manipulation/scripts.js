61930e16b0439f3a980c1c5ab1ca339427ccb665
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-16 09:17:38 -0500

    intial set up for DOM Manipulation

--- javascript-practice/dom-manipulation/scripts.js
+++ javascript-practice/dom-manipulation/scripts.js
@@ -1,1 +1,7 @@
-const log = console.log
+const log = console.log;
+
+box1 = document.getElementById("box1");
+box2 = document.getElementById("box2");
+box3 = document.getElementById("box3");
+
+const numbers = [1, 2, 3, 4, 5];
