372fe328b39bed598a51075cb1e19ffcf2a693e9
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-16 10:29:21 -0500

    added lorem and restore button

--- javascript-practice/dom-manipulation/scripts.js
+++ javascript-practice/dom-manipulation/scripts.js
@@ -4,6 +4,10 @@ box1 = document.getElementById("box1");
 box2 = document.getElementById("box2");
 box3 = document.getElementById("box3");
 clearButton = document.getElementById("clear");
+restoreButton = document.getElementById("restore");
+
+const lorem =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
 
 boxArray = [box1, box2, box3];
 
@@ -13,6 +17,13 @@ const clearBoxes = elementsArray => {
   });
 };
 
+const restoreBoxes = elementsArray => {
+  elementsArray.forEach(element => {
+    element.innerHTML = lorem;
+  });
+};
+
 const numbers = [1, 2, 3, 4, 5];
 
 clearButton.addEventListener("click", () => clearBoxes(boxArray));
+restoreButton.addEventListener("click", () => restoreBoxes(boxArray));
