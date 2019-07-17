b22b803bccd644090872f692c0359ddbe602088d
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-17 12:11:03 -0500

    first draft of menu app

---
+++ javascript-practice/restaurant/app.js
@@ -0,0 +1,64 @@
+const log = console.log;
+
+const dishesElement = document.getElementById("dishes");
+
+const courseButtonsContainer = document.getElementById(
+  "courseButtonsContainer"
+);
+
+const showMenuButton = document.getElementById("showMenu");
+
+const [startersButton, entreesButton, dessertsButton] = [
+  document.getElementById("startersButton"),
+  document.getElementById("entreesButton"),
+  document.getElementById("dessertsButton")
+];
+
+const [starters, entrees, desserts] = [
+  document.getElementsByClassName("starters"),
+  document.getElementsByClassName("entrees"),
+  document.getElementsByClassName("desserts")
+];
+
+const startersArray = [];
+const entreesArray = [];
+const dessertsArray = [];
+
+const updateArrays = (startersArray, entreesArray, dessertsArray) => {
+  for (i = 0; i < starters.length; i++) {
+    startersArray.push(starters[i]);
+  }
+  for (i = 0; i < entrees.length; i++) {
+    entreesArray.push(entrees[i]);
+  }
+  for (i = 0; i < desserts.length; i++) {
+    dessertsArray.push(desserts[i]);
+  }
+};
+
+const createDishElement = dish => {
+  return `<div class="dish ${dish.course.toLowerCase()}">
+    <img src="${dish.imageURL}" alt="${dish.title}">
+    <h4 class="dishTitle">${dish.title}</h4>
+    <p class="dishDesc">${dish.description}</p>
+    </div>`;
+};
+
+const populateDishes = () => {
+  courseButtonsContainer.style.display = "block";
+  dishes.map(dish => {
+    const dishElement = createDishElement(dish);
+    dishesElement.insertAdjacentHTML("beforeend", dishElement);
+  });
+  updateArrays(startersArray, entreesArray, dessertsArray);
+};
+
+const hideCourse = courseArray => {
+  courseArray.forEach(course => {
+    course.style.display = "none";
+  });
+};
+
+showMenuButton.addEventListener("click", () => {
+  populateDishes();
+});
