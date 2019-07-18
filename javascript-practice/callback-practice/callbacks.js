11eec806c9b9e2626607668ce47daddeede82139
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-18 11:05:49 -0500

    first attempt at a promise

--- javascript-practice/callback-practice/callbacks.js
+++ javascript-practice/callback-practice/callbacks.js
@@ -1,40 +1,57 @@
-const log = console.log
+// import { resolve } from "path";
+// import { rejects } from "assert";
 
-const paragraphs = document.getElementsByTagName("p")
+const log = console.log;
+
+const paragraphs = document.getElementsByTagName("p");
 
 const paragraphsArray = [];
 
 const htmlCollectionToArray = (htmlCollection, arr) => {
-    for (index = 0; index < htmlCollection.length; index++) {
-      arr.push(htmlCollection[index]);
-    }
-  };
+  for (index = 0; index < htmlCollection.length; index++) {
+    arr.push(htmlCollection[index]);
+  }
+};
 
-htmlCollectionToArray(paragraphs, paragraphsArray)
+htmlCollectionToArray(paragraphs, paragraphsArray);
 
-const addImg = () => {
-  paragraphsArray.forEach(paragraph => {
-    paragraph.innerHTML += `<img class="image" src="https://i.imgur.com/uXah5QE.jpg">`
-  })
-}
+// const addImg = () => {
+//   paragraphsArray.forEach(paragraph => {
+//     paragraph.innerHTML += `<img class="image" src="https://i.imgur.com/uXah5QE.jpg">`;
+//   });
+// };
 
-// window.setTimeout(sayHello, 5000)
-// window.setInterval(addImg, 10)
+// // window.setTimeout(sayHello, 5000)
+// // window.setInterval(addImg, 10)
 
-const mkCard = () => {
-  log("card made")
-}
-const firstCallback = (callback) => {
-  debugger;
-  secondCallback(callback)
-}
-const secondCallback = (callback) => {
-  callback()
-}
+// const mkCard = () => {
+//   log("card made");
+// };
+// const firstCallback = callback => {
+//   debugger;
+//   secondCallback(callback);
+// };
+// const secondCallback = callback => {
+//   callback();
+// };
 
-const add = (callback) => {
-  callback()
-}
-add(() => {
-  log("callback function called")
-})
+// const add = callback => {
+//   callback();
+// };
+// add(() => {
+//   log("callback function called");
+// });
+
+const fetchPhotos = new Promise((resolve, reject) => {
+  let photos = [];
+  setTimeout(() => {
+    photos = ["photo1", "photo2"];
+  }, 2000);
+  if (photos.length === 2) {
+    resolve(() => {
+      return photos;
+    });
+  } else {
+    reject(Error("photos not loaded"));
+  }
+});
