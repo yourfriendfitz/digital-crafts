b01e257952bf82160bb1b7342dc9e27c8aa257d2
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-18 12:50:58 -0500

    quotes app is working

--- javascript-practice/stock-quotes/stockQuotes.js
+++ javascript-practice/stock-quotes/stockQuotes.js
@@ -9,7 +9,7 @@ const [
 ] = [
   document.getElementById("allQuotes"),
   document.getElementById("appleQuotes"),
-  document.getElementById("amazonlQuotes"),
+  document.getElementById("amazonQuotes"),
   document.getElementById("alibabaQuotes"),
   document.getElementById("googleQuotes"),
   document.getElementById("facebookQuotes"),
@@ -17,7 +17,9 @@ const [
 ];
 
 const quoteElement = quote => {
-  `<div class="quote"><h1>${quote.name}</h1><h3>${quote.price}</h3></div>`;
+  return `<div class="quote" id="${quote.name.toLowerCase()}"><h1>${
+    quote.name
+  }</h1><h3>${quote.price}</h3></div>`;
 };
 
 // In order to get the quotes you can call the getStockQuote function as shown below:
@@ -31,13 +33,15 @@ const quoteElement = quote => {
 // GOOG
 // FB
 
-let quotes = [
-  {APLE: { name: "Apple", price: 0 }},
-  {AMAZ: { name: "Amazon", price: 0 }},
-  {ALBAB: { name: "Ali Baba", price: 0 }},
-  {GOOG: { name: "Google", price: 0 }},
-  {FB: { name: "Facebook", price: 0 }}
-];
+const symbolsArray = ["APLE", "AMAZ", "ALBAB", "GOOG", "FB"];
+
+let quotes = {
+  APLE: { name: "Apple", price: 0 },
+  AMAZ: { name: "Amazon", price: 0 },
+  ALBAB: { name: "Ali Baba", price: 0 },
+  GOOG: { name: "Google", price: 0 },
+  FB: { name: "Facebook", price: 0 }
+};
 
 function getStockQuote(symbol) {
   let stock = quotes[symbol];
@@ -45,14 +49,14 @@ function getStockQuote(symbol) {
   return quotes[symbol];
 }
 
-function getStockQuotes(quotesArray) {
+const getStockQuotes = symbolsArray => {
   const currentQuotes = [];
-  quotesArray.forEach(quote => {
-    quote.price = getRandomInt(100, 500);
+  symbolsArray.forEach(symbol => {
+    const quote = getStockQuote(symbol);
     currentQuotes.push(quote);
   });
   return currentQuotes;
-}
+};
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -77,3 +81,50 @@ const hideQuote = quote => {
 const showQuote = quote => {
   quote.style.display = "flex";
 };
+
+const displayQuotes = (quotesArray, targetDiv) => {
+  quotesArray.forEach(quote => {
+    targetDiv.insertAdjacentHTML("beforeend", quoteElement(quote));
+  });
+};
+
+allQuotesButton.addEventListener("click", () => {
+  quotesDiv.innerHTML = "";
+  const currentQuotes = getStockQuotes(symbolsArray);
+  displayQuotes(currentQuotes, quotesDiv);
+});
+
+appleQuotesButton.addEventListener("click", () => {
+  quotesDiv.innerHTML = "";
+  newQuote = getStockQuote(symbolsArray[0]);
+  newQuoteElement = quoteElement(newQuote);
+  quotesDiv.innerHTML = newQuoteElement;
+});
+
+amazonQuotesButton.addEventListener("click", () => {
+  quotesDiv.innerHTML = "";
+  newQuote = getStockQuote(symbolsArray[1]);
+  newQuoteElement = quoteElement(newQuote);
+  quotesDiv.innerHTML = newQuoteElement;
+});
+
+alibabaQuotesButton.addEventListener("click", () => {
+  quotesDiv.innerHTML = "";
+  newQuote = getStockQuote(symbolsArray[2]);
+  newQuoteElement = quoteElement(newQuote);
+  quotesDiv.innerHTML = newQuoteElement;
+});
+
+googleQuotesButton.addEventListener("click", () => {
+  quotesDiv.innerHTML = "";
+  newQuote = getStockQuote(symbolsArray[3]);
+  newQuoteElement = quoteElement(newQuote);
+  quotesDiv.innerHTML = newQuoteElement;
+});
+
+facebookQuotesButton.addEventListener("click", () => {
+  quotesDiv.innerHTML = "";
+  newQuote = getStockQuote(symbolsArray[4]);
+  newQuoteElement = quoteElement(newQuote);
+  quotesDiv.innerHTML = newQuoteElement;
+});
