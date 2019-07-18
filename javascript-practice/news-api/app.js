5d72b1d6194c881083b8c1d478e32d638a7cd60d
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-18 15:09:00 -0500

    full functionality in script, working on UI

--- javascript-practice/news-api/app.js
+++ javascript-practice/news-api/app.js
@@ -2,34 +2,49 @@ const newsDiv = document.getElementById("news");
 
 const newsArticles = news.articles;
 
-const sourceNames = sources.sources.name
+const sourceNames = sources.sources.name;
 
-const sourceArray = sources.sources
+const sourceArray = sources.sources;
 
 const createArticleElement = (article, index) => {
   return `<div class="article" id="${article.source.id}${index}"><h1>${
     article.title
   }</h1><h2>${article.author}</h2><p>${article.description}</p><a href="${
     article.url
-  }>${article.url}</a><a href="${article.urlToImage}>${
+  }">${article.url}</a><a href="${article.urlToImage}">${
     article.urlToImage
   }</a><h6>${article.publishedAt}</h6></div>`;
 };
 
-const createSourceElement = (source) => {
-    return `<div class="source" id="${source.id}">${source.name}</div>`
-}
+const createSourceElement = source => {
+  return `<div class="source" id="${source.id}">${source.name}</div>`;
+};
+
+const addArticleElement = (article, index, targetDiv) => {
+  const articleElement = createArticleElement(article, index);
+  targetDiv.insertAdjacentHTML("beforeend", articleElement);
+};
 
 const displayArticles = articlesArray => {
   articlesArray.forEach((article, index) => {
-    const articleElement = createArticleElement(article, index);
-    newsDiv.insertAdjacentHTML("beforeend", articleElement);
+    addArticleElement(article, index, newsDiv);
   });
 };
 
 const displaySources = sourcesArray => {
-    sourcesArray.forEach(source => {
-      const sourceElement = createSourceElement(source);
-      newsDiv.insertAdjacentHTML("beforeend", sourceElement);
-    });
-  };
+  sourcesArray.forEach(source => {
+    const sourceElement = createSourceElement(source);
+    newsDiv.insertAdjacentHTML("beforeend", sourceElement);
+  });
+};
+
+const displayBySource = (sourceId, articlesArray) => {
+  articlesArray.forEach((article, index) => {
+    articleSource = article.source.id;
+    if (articleSource != sourceId) {
+      return;
+    } else {
+      addArticleElement(article, index, newsDiv);
+    }
+  });
+};
