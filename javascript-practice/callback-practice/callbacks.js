094d7001f6e22b04c99ecaeef6b61940266d4c05
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-18 10:24:59 -0500

    using setInterval to add images

--- javascript-practice/callback-practice/callbacks.js
+++ javascript-practice/callback-practice/callbacks.js
@@ -1,14 +1,22 @@
 const log = console.log
 
+const paragraphs = document.getElementsByTagName("p")
+
+const paragraphsArray = [];
+
 const htmlCollectionToArray = (htmlCollection, arr) => {
     for (index = 0; index < htmlCollection.length; index++) {
       arr.push(htmlCollection[index]);
     }
   };
 
-const sayHello = () => {
-  log("hello")
+htmlCollectionToArray(paragraphs, paragraphsArray)
+
+const addImg = () => {
+  paragraphsArray.forEach(paragraph => {
+    paragraph.innerHTML += `<img class="image" src="https://i.imgur.com/uXah5QE.jpg">`
+  })
 }
 
-window.setTimeout(sayHello, 5000)
-window.setInterval(sayHello, 5000)
+// window.setTimeout(sayHello, 5000)
+// window.setInterval(addImg, 10)
