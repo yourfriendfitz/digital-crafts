bc24100a1881968052485d3101cdf63d061a40f2
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-18 09:37:01 -0500

    added htmlCollectionToArray function, linked script to html

--- javascript-practice/callback-practice/callbacks.js
+++ javascript-practice/callback-practice/callbacks.js
@@ -1,1 +1,7 @@
 const log = console.log
+
+const htmlCollectionToArray = (htmlCollection, arr) => {
+    for (index = 0; index < htmlCollection.length; index++) {
+      arr.push(htmlCollection[index]);
+    }
+  };
