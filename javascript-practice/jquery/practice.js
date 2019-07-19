5c01f6fd1f4dcf421b9d58054c3a68561c0ed6ed
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-19 10:47:52 -0500

    added more styling

--- javascript-practice/jquery/practice.js
+++ javascript-practice/jquery/practice.js
@@ -1,1 +1,11 @@
-const log = console.log
+const log = console.log;
+
+$(document).ready(() => {
+  const taskInput = $("#taskInput");
+  const addButton = $("#addTask");
+  const taskList = $("#taskList");
+  addButton.click(() => {
+    const taskName = `<div class="task">${taskInput.val()}</div>`;
+    taskList.append(taskName);
+  });
+});
