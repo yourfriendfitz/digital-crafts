c29530d778ab44b5314f69e8dd2afb6e7e09c52b
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-19 12:03:35 -0500

    finished jquery fixes

--- javascript-practice/todo-jquery/todo.js
+++ javascript-practice/todo-jquery/todo.js
@@ -1,2 +1,80 @@
-const log = console.log
+const log = console.log;
 
+const taskInput = $("#enter-task-input");
+
+const addTaskButton = $("#add-task");
+
+const removeTaskButtons = $(".remove");
+
+const pendingTaskContainer = $("#pending");
+
+const compTaskContainer = $("#completed");
+
+let taskToAdd;
+
+let checkboxVisible = false;
+
+const addPendingTask = taskName => {
+  const task = `<div class="task">
+      <input type="checkbox" onclick="moveToComp(this)">
+      <h5>${taskName}</h5>
+      <button class="remove" onclick="removeTask(this)">Remove</button>
+    </div>`;
+  pendingTaskContainer.append(task);
+};
+
+const showTaskPreview = taskName => {
+  const task = `<div class="task" id="preview" style="opacity: 0.5">
+      <input type="checkbox">
+      <h5>${taskName}</h5>
+      <button class="remove" onclick="removeTask(this)">Remove</button>
+    </div>`;
+  pendingTaskContainer.append(task);
+};
+
+const removeTask = e => {
+  taskToRemove = e.parentNode;
+  taskToRemove.remove();
+};
+
+const addCompTask = taskName => {
+  const task = `<div class="task">
+        <input type="checkbox" onclick="moveToPend(this)" checked>
+        <h5>${taskName}</h5>
+        <button class="remove" onclick="removeTask(this)">Remove</button>
+      </div>`;
+  compTaskContainer.append(task);
+};
+
+const moveToComp = e => {
+  const compTaskName = e.parentNode.childNodes[3].innerHTML;
+  removeTask(e);
+  addCompTask(compTaskName);
+};
+
+const moveToPend = e => {
+  const compTaskName = e.parentNode.childNodes[3].innerHTML;
+  removeTask(e);
+  addPendingTask(compTaskName);
+};
+
+const removePreview = () => {
+  if ($("#preview")) {
+    $("#preview").remove();
+  }
+};
+
+taskInput.on("input", () => {
+  removePreview();
+  const taskToPreview = taskInput.val();
+  showTaskPreview(taskToPreview);
+});
+
+taskInput.on("change", () => {
+  taskToAdd = taskInput.val();
+});
+
+addTaskButton.click(() => {
+  removePreview();
+  addPendingTask(taskToAdd);
+});
