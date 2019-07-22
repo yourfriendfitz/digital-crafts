581e19e1db28ce980c85cf5a92dd1c29c7f5d4da
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-22 13:59:03 -0500

    github was down

--- api-practice/app.js
+++ api-practice/app.js
@@ -1,15 +1,40 @@
 const log = console.log;
 
-let url =
-  "https://dictionaryapi.com/api/v3/references/collegiate/json/test?key=a7aaf907-e0d5-4865-8ec5-2145a2c443d5";
+const queryInput = document.getElementById("query");
 
-const Get = (yourUrl) => {
-  var Httpreq = new XMLHttpRequest(); // a new request
-  Httpreq.open("GET", yourUrl, false);
-  Httpreq.send(null);
-  console.log(Httpreq)
-  return Httpreq.responseText;
-}
+const queryButton = document.getElementById("addWord");
 
-let jsonObj = JSON.parse(Get(url))
-log(jsonObj)
+const wordList = document.getElementById("wordList");
+
+let url = "";
+let jsonObj = {};
+
+const createWord = jsonObject => {
+  return `
+    <div class="word">
+    <h6>${jsonObject[0].meta.stems[0]}</h6>
+    <p class="definition">${jsonObject[0].shortdef}</p>
+    </div>
+    `;
+};
+
+queryInput.addEventListener("change", () => {
+  const query = queryInput.value;
+  url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${query}?key=a7aaf907-e0d5-4865-8ec5-2145a2c443d5`;
+});
+
+queryButton.addEventListener("click", () => {
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(jsonData) {
+      console.log(jsonData);
+      jsonObj = jsonData;
+      const newWord = createWord(jsonObj);
+      wordList.insertAdjacentHTML("afterbegin", newWord);
+    })
+    .catch(function(err) {
+      console.log("Opps, Something went wrong!", err);
+    });
+});
