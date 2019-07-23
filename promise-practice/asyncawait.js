10aa0436119f17ea9d73a09595f888aac55dcce6
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-23 11:08:27 -0500

    added fetch POST method

--- promise-practice/asyncawait.js
+++ promise-practice/asyncawait.js
@@ -3,10 +3,25 @@ const log = console.log;
 let movieUrl = "http://www.omdbapi.com/?s=batman&apikey=26332775";
 
 const asyncFetch = async () => {
-    let response = await fetch(movieUrl)
-    let json = await response.json()
-    log(response)
-    log(json)
-}
+  let response = await fetch(movieUrl);
+  let json = await response.json();
+  log(response);
+  log(json);
+};
 
-asyncFetch()
+asyncFetch();
+
+const asyncFetchPost = async () => {
+  let post = await fetch("https://reqres.in/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      name: "Fitz",
+      job: "Software Developer"
+    })
+  });
+  let response = await post.json();
+  await log(post)
+  log(response);
+};
+
+asyncFetchPost();
