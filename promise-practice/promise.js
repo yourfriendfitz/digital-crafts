93c0d92fa60323087ad77baacdad2e74165aa9ed
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-23 10:36:33 -0500

    using promise.all

--- promise-practice/promise.js
+++ promise-practice/promise.js
@@ -1,19 +1,44 @@
 const log = console.log;
 
-let promise = new Promise((resolve, reject) => {
-  let arr = [1, 2];
+// let promise = new Promise((resolve, reject) => {
+//   let arr = [1, 2];
+//   setTimeout(() => {
+//     resolve(arr);
+//   }, 5000);
+// });
+
+// promise
+//   .then(arr => {
+//     log(arr);
+//     return arr;
+//   })
+//   .then(arr => {
+//     setTimeout(() => {
+//       log(`second then ${arr}`);
+//     }, 2000);
+//   })
+//   .catch(err => {
+//     log(err);
+//   });
+
+const bitcoin = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve(arr);
-  }, 5000);
+    resolve("BITCOIN");
+  }, 500);
 });
 
-promise
-  .then(arr => {
-    log(arr);
-    return arr;
-  })
-  .then(arr => {
-    setTimeout(() => {
-      log(`second then ${arr}`);
-    }, 2000);
-  });
+const ether = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("ETHERIUM");
+  }, 700);
+});
+
+const lite = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("LITECOIN");
+  }, 1000);
+});
+
+Promise.all([bitcoin, ether, lite]).then(data => {
+  log(data);
+});
