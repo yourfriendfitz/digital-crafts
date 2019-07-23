ec51128ddf4e88305d094663f41219f0b1afdd1c
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-23 10:23:38 -0500

    added two .then()

--- promise-practice/promise.js
+++ promise-practice/promise.js
@@ -1,5 +1,19 @@
-const log = console.log
+const log = console.log;
 
 let promise = new Promise((resolve, reject) => {
-  
-})
+  let arr = [1, 2];
+  setTimeout(() => {
+    resolve(arr);
+  }, 5000);
+});
+
+promise
+  .then(arr => {
+    log(arr);
+    return arr;
+  })
+  .then(arr => {
+    setTimeout(() => {
+      log(`second then ${arr}`);
+    }, 2000);
+  });
