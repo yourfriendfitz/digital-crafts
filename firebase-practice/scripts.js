36d8477c7f6f00b1d4428d0100e7b7bb2135c926
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-24 11:38:33 -0500

    removed write test

--- firebase-practice/scripts.js
+++ firebase-practice/scripts.js
@@ -5,19 +5,6 @@ firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 
-// db.collection("groceries")
-//   .add({
-//     first: "Ada",
-//     last: "Lovelace",
-//     born: 1815
-//   })
-//   .then(function(docRef) {
-//     console.log("Document written with ID: ", docRef.id);
-//   })
-//   .catch(function(error) {
-//     console.error("Error adding document: ", error);
-//   });
-
 db.collection("groceries")
   .doc("HEB")
   .set({
