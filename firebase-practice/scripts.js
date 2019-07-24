3371d515176c39954341f4622bf1d2ae7d9bb401
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-24 10:47:12 -0500

    add data to firestore

--- firebase-practice/scripts.js
+++ firebase-practice/scripts.js
@@ -1,1 +1,28 @@
 const log = console.log;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZq-6vpmc4Vl5d2aUJDFjHrBUWHKM0iz0",
+  authDomain: "fitz-grocery.firebaseapp.com",
+  databaseURL: "https://fitz-grocery.firebaseio.com",
+  projectId: "fitz-grocery",
+  storageBucket: "fitz-grocery.appspot.com",
+  messagingSenderId: "10762804784",
+  appId: "1:10762804784:web:0e060d1ed05db970"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+db.collection("groceries")
+  .add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  })
+  .then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  });
