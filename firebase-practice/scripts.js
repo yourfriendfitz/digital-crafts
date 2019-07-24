b7726ea45f44c336624d39d41ad94a8227859fcc
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-24 11:11:27 -0500

    testing write functionality

--- firebase-practice/scripts.js
+++ firebase-practice/scripts.js
@@ -5,15 +5,31 @@ firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 
+// db.collection("groceries")
+//   .add({
+//     first: "Ada",
+//     last: "Lovelace",
+//     born: 1815
+//   })
+//   .then(function(docRef) {
+//     console.log("Document written with ID: ", docRef.id);
+//   })
+//   .catch(function(error) {
+//     console.error("Error adding document: ", error);
+//   });
+
 db.collection("groceries")
-  .add({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
+  .doc("HEB")
+  .set({
+    items: [
+      { item: "eggs", price: 2.0 },
+      { item: "milk", price: 1.5 },
+      { item: "bread", price: 0.5 }
+    ]
   })
   .then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
+    console.log("Document written");
   })
   .catch(function(error) {
-    console.error("Error adding document: ", error);
+    console.error("Error writing document: ", error);
   });
