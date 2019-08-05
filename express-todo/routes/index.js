9a2c7f4688400bf346cba7bc792a02e873bf8297
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-05 13:18:17 -0500

    added add task functionality

--- express-todo/routes/index.js
+++ express-todo/routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+let pendingTasks = [];
+let completedTasks = [];
+
+class Task {
+  constructor(name) {
+    this.taskName = name;
+  }
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", {
+    title: "Express Todo",
+    pendingTasks: pendingTasks,
+    completedTasks: completedTasks
+  });
+});
+
+router.post("/", function(req, res) {
+  const task = new Task(req.body.taskName)
+  pendingTasks.push(task)
+  res.redirect("/")
 });
 
 module.exports = router;
