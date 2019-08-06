288fdb2897a2022794d21e735db90ef9ddb6c23c
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-06 12:09:45 -0500

    added trip functionality

--- express-trips/routes/index.js
+++ express-trips/routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const Trip = require("../public/javascripts/modules/trip");
+
+let trips = [];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", {
+    title: "Express Trips",
+    trips: trips
+  });
+});
+
+router.post("/addTrip", function(req, res) {
+  const trip = new Trip(
+    {
+      city: req.body.city,
+      state: req.body.state
+    },
+    req.body.url,
+    req.body.begin,
+    req.body.return
+  );
+  trips.push(trip);
+  res.redirect("/");
 });
 
 module.exports = router;
