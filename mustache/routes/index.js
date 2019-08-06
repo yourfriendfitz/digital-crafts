d309fa9c1dd7ff9bbb29494277def41cd080eda3
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-06 11:09:10 -0500

    add user form working

--- mustache/routes/index.js
+++ mustache/routes/index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
+const users = [{ name: "John", age: 21 }, { name: "Mary", age: 25 }];
+
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Hello Mustache", name: "Fitz" });
@@ -12,8 +14,17 @@ router.get("/animals", function(req, res, next) {
 });
 
 router.get("/users", function(req, res, next) {
-  const users = [{ name: "John", age: 21 }, { name: "Mary", age: 25 }];
   res.render("users", { users: users });
+});
+
+router.get("/addUser", function(req, res, next) {
+  res.render("addUser");
+});
+
+router.post("/addUser", function(req, res, next) {
+  const [name, age] = [req.body.name, req.body.age];
+  users.push({ name: name, age: age });
+  res.redirect("/addUser");
 });
 
 module.exports = router;
