0783ffaa9ed7fd7be58d4a555df434fffd3d525e
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-06 10:57:25 -0500

    added user and animals

--- mustache/routes/index.js
+++ mustache/routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello Mustache', name: "Fitz" });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Hello Mustache", name: "Fitz" });
+});
+
+router.get("/animals", function(req, res, next) {
+  const animals = ["dog", "cat", "monkey", "kangaroo"];
+  res.render("animals", { animals: animals });
+});
+
+router.get("/users", function(req, res, next) {
+  const users = [{ name: "John", age: 21 }, { name: "Mary", age: 25 }];
+  res.render("users", { users: users });
 });
 
 module.exports = router;
