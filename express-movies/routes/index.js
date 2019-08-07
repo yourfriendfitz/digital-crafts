2063f0a50518306cd6db647e78d8d12d9fe72674
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-07 12:30:52 -0500

    add movie route working

--- express-movies/routes/index.js
+++ express-movies/routes/index.js
@@ -6,4 +6,15 @@ router.get("/", function(req, res, next) {
   res.render("index", { title: "Express Movies", movies: movies });
 });
 
+router.post("/addMovie", function(req, res, next) {
+  const movie = new Movie(
+    req.body.title,
+    req.body.year,
+    req.body.genre,
+    req.body.url
+  );
+  movies.push(movie);
+  res.redirect("/");
+});
+
 module.exports = router;
