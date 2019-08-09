3e9df7a5e79ea7b50bfc3ef5e8eab6e30ca8b727
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-09 12:06:23 -0500

    style changes

--- chatroom-socket/index.js
+++ chatroom-socket/index.js
@@ -1,6 +1,9 @@
 var app = require("express")();
+var express = require("express");
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
+
+app.use(express.static(__dirname));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
