f6acc2d6279585da137f643385bdf9de07cc5ccb
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-14 11:02:15 -0500

    added username column

--- sequelize/models/trip.js
+++ sequelize/models/trip.js
@@ -1,9 +1,14 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Trip = sequelize.define('Trip', {
-    name: DataTypes.STRING,
-    description: DataTypes.STRING
-  }, {});
+  const Trip = sequelize.define(
+    "Trip",
+    {
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      username: DataTypes.STRING
+    },
+    {}
+  );
   Trip.associate = function(models) {
     // associations can be defined here
   };
