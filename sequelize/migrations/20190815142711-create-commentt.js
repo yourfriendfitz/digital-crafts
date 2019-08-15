f4de57d3bd0bf78bdad8eee2fb2d00e9b4ed63c3
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-15 09:32:27 -0500

    adding fk to comments

---
+++ sequelize/migrations/20190815142711-create-commentt.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      subject: {
+        type: Sequelize.STRING
+      },
+      body: {
+        type: Sequelize.STRING
+      },
+      postId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Comments');
+  }
+};
---
+++ sequelize/models/comment.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Comment = sequelize.define('Comment', {
+    subject: DataTypes.STRING,
+    body: DataTypes.STRING,
+    postId: DataTypes.INTEGER
+  }, {});
+  Comment.associate = function(models) {
+    // associations can be defined here
+  };
+  return Comment;
+};
