0d46df445505ec7d974b5367207f7792b9505fed
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-15 09:49:45 -0500

    added create-post route

--- sequelize/app.js
+++ sequelize/app.js
@@ -4,49 +4,18 @@ const models = require("./models");
 
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/add-trip", async (req, res) => {
-  const name = req.body.name;
-  const description = req.body.description;
-  const trip = models.Trip.build({
-    name: name,
-    description: description
+app.post("/create-post", async (req, res) => {
+  const title = req.body.title;
+  const body = req.body.body;
+  const isPublished = req.body.isPublished == "true" ? true : false;
+
+  const post = models.Post.build({
+    title: title,
+    body: body,
+    isPublished: isPublished
   });
-
-  await trip.save();
-  res.send("TRIP");
-});
-
-app.get("/trips", async (req, res) => {
-  const trips = await models.Trip.findAll();
-  res.json(trips);
-});
-
-app.get("/trips/:tripId", async (req, res) => {
-  const tripId = req.params.tripId;
-  const tripToDelete = await models.Trip.destroy({
-    where: {
-      id: tripId
-    }
-  });
-  res.json(tripToDelete);
-});
-
-app.post("/update-trip", async (req, res) => {
-  const name = req.body.name;
-  const description = req.body.description;
-  const tripId = parseInt(req.body.tripId);
-  const update = await models.Trip.update(
-    {
-      name: name,
-      description: description
-    },
-    {
-      where: {
-        id: tripId
-      }
-    }
-  );
-  res.json(update)
+  const response = await post.save();
+  res.json(response);
 });
 
 app.listen(3000);
