7ad6e5a6ae4d972b218d13b855d70938c8c794dc
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-15 10:26:31 -0500

    added one to many to Post class

--- sequelize/models/post.js
+++ sequelize/models/post.js
@@ -1,12 +1,21 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Post = sequelize.define('Post', {
-    title: DataTypes.STRING,
-    body: DataTypes.STRING,
-    isPublished: DataTypes.BOOLEAN
-  }, {});
+  const Post = sequelize.define(
+    "Post",
+    {
+      title: DataTypes.STRING,
+      body: DataTypes.STRING,
+      isPublished: DataTypes.BOOLEAN
+    },
+    {}
+  );
   Post.associate = function(models) {
     // associations can be defined here
+    // create one to many relatioship between posts and comments
+    models.Post.hasMany(models.Comment, {
+      as: "comments",
+      foreignKey: "postId"
+    });
   };
   return Post;
 };
