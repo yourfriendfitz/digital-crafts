29f386e76de3b07c404c7aac8931eefb60d0685d
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-15 10:26:44 -0500

    formatted

--- sequelize/models/comment.js
+++ sequelize/models/comment.js
@@ -1,10 +1,14 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Comment = sequelize.define('Comment', {
-    subject: DataTypes.STRING,
-    body: DataTypes.STRING,
-    postId: DataTypes.INTEGER
-  }, {});
+  const Comment = sequelize.define(
+    "Comment",
+    {
+      subject: DataTypes.STRING,
+      body: DataTypes.STRING,
+      postId: DataTypes.INTEGER
+    },
+    {}
+  );
   Comment.associate = function(models) {
     // associations can be defined here
   };
