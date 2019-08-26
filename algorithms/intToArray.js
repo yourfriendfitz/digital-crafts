e8208c4c09ab2d11f99cbafb46a3e1dc5beb5bd3
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-08-26 14:59:49 -0500

    solved

--- algorithms/intToArray.js
+++ algorithms/intToArray.js
@@ -1,2 +1,13 @@
-let num = 234
+let num = 234;
 
+const intToDigitsArray = num => {
+  var digits = [];
+  while (num > 0) {
+    digits.push(num % 10);
+    num = parseInt(num / 10);
+  }
+  digits.reverse();
+  return digits;
+};
+
+intToDigitsArray(num);
