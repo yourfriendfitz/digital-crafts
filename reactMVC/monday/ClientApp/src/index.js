dc9238d5e1491562833f8c7d42445a5f0599761d
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-09-07 21:19:11 -0500

    initial theming

--- reactMVC/monday/ClientApp/src/index.js
+++ reactMVC/monday/ClientApp/src/index.js
@@ -4,19 +4,19 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import styled from "styled-components";
-import { Primary } from "./components/Palette";
+import * as Palette from "./components/Palette";
 import registerServiceWorker from "./registerServiceWorker";
 
 const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
 const rootElement = document.getElementById("root");
 
 const GrayDiv = styled.div`
-  background-color: ${Primary};
+  background-color: ${Palette.Primary};
 `;
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
-    <GrayDiv>
+    <GrayDiv className="container-fluid overflow-auto vh-100">
       <App />
     </GrayDiv>
   </BrowserRouter>,
