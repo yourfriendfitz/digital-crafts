7455e71e635d8954db7847680a11a9a45aaa1e99
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-09-09 10:50:41 -0500

    added reducer

--- redux/helloRedux.js
+++ redux/helloRedux.js
@@ -1,14 +1,27 @@
-
-
-// store
-
+const redux = require("redux");
+const createStore = redux.createStore;
 
 // reducer
+    // create initial state
+const initialState = {
+  counter: 0
+};
+    //  use initialState
+const reducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case "INCREMENT":
+      return { ...state, ...payload };
 
+    default:
+      return state;
+  }
+};
+
+// store
+const store = createStore(reducer);
 
 // dispatch actions
 
-
 // subscriptions
 
-console.log("Hello Redux")
+console.log("Hello Redux");
