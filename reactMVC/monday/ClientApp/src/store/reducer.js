28cb1a65d2a7d89484aa320b398e5b93c12dbb12
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-09-10 14:14:48 -0500

    added reducer

---
+++ reactMVC/monday/ClientApp/src/store/reducer.js
@@ -0,0 +1,20 @@
+const initialState = {
+  isAuth: false,
+  cart: []
+};
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    case "ADD":
+      state.cart = [...state.cart, payload];
+      return { ...state, ...payload };
+
+    case "DEL":
+      const newCart = state.cart.filter(item => item.id != payload);
+      state.cart = [...newCart];
+      return { ...state, ...payload };
+
+    default:
+      return state;
+  }
+};
