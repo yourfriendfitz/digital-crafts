433521a90925919b019ea248b7b4c2aec302d4f0
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-09-10 14:15:02 -0500

    added cart link to nav

--- reactMVC/monday/ClientApp/src/components/NavMenu.js
+++ reactMVC/monday/ClientApp/src/components/NavMenu.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import {
   Collapse,
   Container,
@@ -8,10 +8,11 @@ import {
   NavItem,
   NavLink
 } from "reactstrap";
-import * as Palette from "./Palette"
+import * as Palette from "./Palette";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import "./NavMenu.css";
+import { connect } from "react-redux";
 
 const Text = styled.span`
   color: ${Palette.Text};
@@ -21,65 +22,67 @@ const TextDark = styled.span`
   color: ${Palette.Text};
 `;
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+const NavMenu = props => {
+  const [collapsed, setCollapsed] = useState(false);
 
-  constructor(props) {
-    super(props);
+  const toggleNavbar = () => {
+    setCollapsed(!collapsed);
+  };
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
+  return (
+    <header>
+      <Navbar
+        className="navbar-expand-sm navbar-toggleable-sm ng-white box-shadow mb-3"
+        light
+      >
+        <Container>
+          <NavbarBrand tag={Link} to="/">
+            <span aria-label="none" role="img">
+              <TextDark>Book Barn</TextDark>
+              📚
+            </span>
+          </NavbarBrand>
+          <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+          <Collapse
+            className="d-sm-inline-flex flex-sm-row-reverse"
+            isOpen={!collapsed}
+            navbar
+          >
+            <ul className="navbar-nav flex-grow">
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/">
+                  <Text>Home</Text>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/books">
+                  <Text>Books</Text>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/books/add">
+                  <Text>Utilities</Text>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/cart">
+                  <Text>
+                    Cart {props.cart.length}
+                  </Text>
+                </NavLink>
+              </NavItem>
+            </ul>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
 
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
+const mapStateToProps = state => {
+  return {
+    cart: state.cart // props.count
+  };
+};
 
-  render() {
-    return (
-      <header>
-        <Navbar
-          className="navbar-expand-sm navbar-toggleable-sm ng-white box-shadow mb-3"
-          light
-        >
-          <Container>
-            <NavbarBrand tag={Link} to="/">
-              <span aria-label="none" role="img">
-                <TextDark>Book Barn</TextDark>
-                📚
-              </span>
-            </NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse
-              className="d-sm-inline-flex flex-sm-row-reverse"
-              isOpen={!this.state.collapsed}
-              navbar
-            >
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">
-                    <Text>Home</Text>
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/books">
-                    <Text>Books</Text>
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/books/add">
-                    <Text>Utilities</Text>
-                  </NavLink>
-                </NavItem>
-              </ul>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    );
-  }
-}
+export default connect(mapStateToProps)(NavMenu);
