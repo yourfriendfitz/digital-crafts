c6b9839d034fcd17becc83324d0f047d3f75923d
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-09-10 14:13:39 -0500

    redux added

--- reactMVC/monday/ClientApp/src/index.js
+++ reactMVC/monday/ClientApp/src/index.js
@@ -6,20 +6,33 @@ import App from "./App";
 import styled from "styled-components";
 import * as Palette from "./components/Palette";
 import registerServiceWorker from "./registerServiceWorker";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./store/reducer";
 
 const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
 const rootElement = document.getElementById("root");
 
 const Container = styled.div`
-  background-color: ${Palette.Primary};
+  background-image: linear-gradient(
+    to right bottom,
+    ${Palette.Primary},
+    ${Palette.AltPrimary}
+  );
   display: grid;
-
 `;
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
     <Container className="overflow-auto p-0 m-0 vh-100">
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Container>
   </BrowserRouter>,
   rootElement
