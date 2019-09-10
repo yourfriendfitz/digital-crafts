750a698f74fdec465ed1b1feb6c2eda91ca51338
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-09-10 14:15:25 -0500

    updated button for books

--- reactMVC/monday/ClientApp/src/components/Books.js
+++ reactMVC/monday/ClientApp/src/components/Books.js
@@ -1,8 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Card, CardImg, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
 import styled from "styled-components";
-import * as Palette from "./Palette"
+import * as Palette from "./Palette";
 import { Container as BootstrapContainer } from "reactstrap";
+import { connect } from "react-redux";
 
 const List = styled.div`
   display: grid;
@@ -23,25 +31,30 @@ const StyledButton = styled(Button)`
 `;
 
 const ColoredCard = styled(Card)`
-background-color: ${Palette.Secondary}
-`
-
-const ColoredCardTitle = styled(CardTitle)`
-color: ${Palette.Text}
-`
-
-const ColoredCardSubtitle = styled(CardSubtitle)`
-color: ${Palette.Text}
-`
-
-const CardContainer = styled.div`
+  background-image: linear-gradient(
+    to right bottom,
+    ${Palette.Secondary},
+    ${Palette.AltPrimary}
+  );
+  box-shadow: 0 2rem 4rem rgba(0, 0, 0, 0.2);
+  border-radius: 8px;
 `;
 
-const Container = styled(BootstrapContainer)`
-max-width: 350px;
-`
+const ColoredCardTitle = styled(CardTitle)`
+  color: ${Palette.Text};
+`;
 
-const Books = () => {
+const ColoredCardSubtitle = styled(CardSubtitle)`
+  color: ${Palette.Text};
+`;
+
+const CardContainer = styled.div``;
+
+const Container = styled(BootstrapContainer)`
+  max-width: 350px;
+`;
+
+const Books = props => {
   const [data, setData] = useState([]);
   useEffect(() => {
     fetch("https://boiling-escarpment-07603.herokuapp.com/books")
@@ -62,13 +75,18 @@ const Books = () => {
               />
               <CardBody className="text-center">
                 <ColoredCardTitle>{book.title}</ColoredCardTitle>
-                <ColoredCardSubtitle className="m-2">{book.author}</ColoredCardSubtitle>
+                <ColoredCardSubtitle className="m-2">
+                  {book.author}
+                </ColoredCardSubtitle>
                 <Link
                   target="_blank"
                   href={`https://en.wikipedia.org/wiki/${book.title}`}
                 >
                   <StyledButton>Wikipedia</StyledButton>
                 </Link>
+                <StyledButton onClick={() => props.onAdd(book)}>
+                  Add to Cart
+                </StyledButton>
               </CardBody>
             </ColoredCard>
           </CardContainer>
@@ -77,4 +95,19 @@ const Books = () => {
     </Container>
   );
 };
-export default Books;
+
+const addAction = payload => ({
+  type: "ADD",
+  payload
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAdd: (book) => dispatch(addAction(book))
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Books);
