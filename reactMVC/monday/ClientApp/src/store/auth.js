b1ef4ceb3653b2bbac84c67c1d05a87e28c51011
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-09-11 10:50:42 -0500

    split reducer

---
+++ reactMVC/monday/ClientApp/src/store/auth.js
@@ -0,0 +1,20 @@
+import * as actionTypes from "./actionTypes";
+const initialState = {
+  isAuth: false
+};
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    case actionTypes.ADD:
+      state.cart = [...state.cart, payload];
+      return { ...state, ...payload };
+
+    case actionTypes.DEL:
+      const newCart = state.cart.filter(item => item.id !== payload);
+      state.cart = [...newCart];
+      return { ...state, ...payload };
+
+    default:
+      return state;
+  }
+};
